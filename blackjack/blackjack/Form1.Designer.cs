b39262e715a3644249a998f7b46474b9e6d682c7
Pedro Sanches <pedrosanches.art@gmail.com>
2024-11-10 16:59:10 -0300

    fist commit

---
+++ blackjack/blackjack/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace blackjack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_mao_jogador = new System.Windows.Forms.Label();
+            this.btn_hit = new System.Windows.Forms.Button();
+            this.btn_stand = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.lb_pontos_jogador = new System.Windows.Forms.Label();
+            this.lb_mao_dealer = new System.Windows.Forms.Label();
+            this.flp_mao_dealer = new System.Windows.Forms.FlowLayoutPanel();
+            this.flp_mao_jogador = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb_mao_jogador
+            // 
+            this.lb_mao_jogador.AutoSize = true;
+            this.lb_mao_jogador.Location = new System.Drawing.Point(290, 396);
+            this.lb_mao_jogador.Name = "lb_mao_jogador";
+            this.lb_mao_jogador.Size = new System.Drawing.Size(35, 13);
+            this.lb_mao_jogador.TabIndex = 0;
+            this.lb_mao_jogador.Text = "label1";
+            this.lb_mao_jogador.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btn_hit
+            // 
+            this.btn_hit.Location = new System.Drawing.Point(617, 415);
+            this.btn_hit.Name = "btn_hit";
+            this.btn_hit.Size = new System.Drawing.Size(75, 23);
+            this.btn_hit.TabIndex = 1;
+            this.btn_hit.Text = "Hit!";
+            this.btn_hit.UseVisualStyleBackColor = true;
+            this.btn_hit.Click += new System.EventHandler(this.btn_hit_Click);
+            // 
+            // btn_stand
+            // 
+            this.btn_stand.Location = new System.Drawing.Point(617, 386);
+            this.btn_stand.Name = "btn_stand";
+            this.btn_stand.Size = new System.Drawing.Size(75, 23);
+            this.btn_stand.TabIndex = 2;
+            this.btn_stand.Text = "Stand";
+            this.btn_stand.UseVisualStyleBackColor = true;
+            this.btn_stand.Click += new System.EventHandler(this.btn_stand_Click);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Location = new System.Drawing.Point(617, 18);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(75, 23);
+            this.btn_reset.TabIndex = 3;
+            this.btn_reset.Text = "Reset";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // lb_pontos_jogador
+            // 
+            this.lb_pontos_jogador.AutoSize = true;
+            this.lb_pontos_jogador.Location = new System.Drawing.Point(290, 428);
+            this.lb_pontos_jogador.Name = "lb_pontos_jogador";
+            this.lb_pontos_jogador.Size = new System.Drawing.Size(35, 13);
+            this.lb_pontos_jogador.TabIndex = 5;
+            this.lb_pontos_jogador.Text = "label1";
+            // 
+            // lb_mao_dealer
+            // 
+            this.lb_mao_dealer.AutoSize = true;
+            this.lb_mao_dealer.Location = new System.Drawing.Point(290, 134);
+            this.lb_mao_dealer.Name = "lb_mao_dealer";
+            this.lb_mao_dealer.Size = new System.Drawing.Size(35, 13);
+            this.lb_mao_dealer.TabIndex = 6;
+            this.lb_mao_dealer.Text = "label1";
+            // 
+            // flp_mao_dealer
+            // 
+            this.flp_mao_dealer.AutoSize = true;
+            this.flp_mao_dealer.BackColor = System.Drawing.Color.Transparent;
+            this.flp_mao_dealer.Location = new System.Drawing.Point(293, 18);
+            this.flp_mao_dealer.Name = "flp_mao_dealer";
+            this.flp_mao_dealer.Size = new System.Drawing.Size(200, 100);
+            this.flp_mao_dealer.TabIndex = 8;
+            this.flp_mao_dealer.Paint += new System.Windows.Forms.PaintEventHandler(this.flp_mao_dealer_Paint);
+            // 
+            // flp_mao_jogador
+            // 
+            this.flp_mao_jogador.AutoSize = true;
+            this.flp_mao_jogador.BackColor = System.Drawing.Color.Transparent;
+            this.flp_mao_jogador.Location = new System.Drawing.Point(293, 275);
+            this.flp_mao_jogador.Name = "flp_mao_jogador";
+            this.flp_mao_jogador.Size = new System.Drawing.Size(200, 100);
+            this.flp_mao_jogador.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::blackjack.Properties.Resources.Deck;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::blackjack.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.flp_mao_jogador);
+            this.Controls.Add(this.flp_mao_dealer);
+            this.Controls.Add(this.lb_mao_dealer);
+            this.Controls.Add(this.lb_pontos_jogador);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.btn_stand);
+            this.Controls.Add(this.btn_hit);
+            this.Controls.Add(this.lb_mao_jogador);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_mao_jogador;
+        private System.Windows.Forms.Button btn_hit;
+        private System.Windows.Forms.Button btn_stand;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Label lb_pontos_jogador;
+        private System.Windows.Forms.Label lb_mao_dealer;
+        private System.Windows.Forms.FlowLayoutPanel flp_mao_dealer;
+        private System.Windows.Forms.FlowLayoutPanel flp_mao_jogador;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ blackjack/blackjack/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace blackjack
+{
+    public partial class Form1 : Form
+    {
+
+        public class Carta
+        {
+            public string Nome { get; set; }
+            public int Valor { get; set; }
+
+            public System.Drawing.Image Imagem { get; set; }
+
+            public Carta(string nome, int valor, System.Drawing.Image imagem)
+            {
+                Nome = nome;
+                Valor = valor;
+                Imagem = imagem;
+            }
+        }
+        public class Baralho
+        {
+            private List<Carta> cartas;
+            private Random random = new Random();
+            private string basePath;
+
+            public Baralho()
+            {
+                basePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
+                ReporCartas(); // Inicializa o baralho com todas as cartas
+            }
+
+            // Método para reabastecer e embaralhar o baralho
+            public void ReporCartas()
+            {
+                cartas = new List<Carta>
+                {
+                    // Espadas
+                    new Carta("Ás de Espadas", 11, System.Drawing.Image.FromFile(basePath + "imagens/1.png")),
+                    new Carta("Dois de Espadas", 2, System.Drawing.Image.FromFile(basePath + "imagens/5.png")),
+                    new Carta("Três de Espadas", 3, System.Drawing.Image.FromFile(basePath + "imagens/9.png")),
+                    new Carta("Quatro de Espadas", 4, System.Drawing.Image.FromFile(basePath + "imagens/13.png")),
+                    new Carta("Cinco de Espadas", 5, System.Drawing.Image.FromFile(basePath + "imagens/17.png")),
+                    new Carta("Seis de Espadas", 6, System.Drawing.Image.FromFile(basePath + "imagens/21.png")),
+                    new Carta("Sete de Espadas", 7, System.Drawing.Image.FromFile(basePath + "imagens/25.png")),
+                    new Carta("Oito de Espadas", 8, System.Drawing.Image.FromFile(basePath + "imagens/29.png")),
+                    new Carta("Nove de Espadas", 9, System.Drawing.Image.FromFile(basePath + "imagens/33.png")),
+                    new Carta("Dez de Espadas", 10, System.Drawing.Image.FromFile(basePath + "imagens/37.png")),
+                    new Carta("Valete de Espadas", 10, System.Drawing.Image.FromFile(basePath + "imagens/41.png")),
+                    new Carta("Rainha de Espadas", 10, System.Drawing.Image.FromFile(basePath + "imagens/45.png")),
+                    new Carta("Rei de Espadas", 10, System.Drawing.Image.FromFile(basePath + "imagens/49.png")),
+
+                    // Paus
+                    new Carta("Ás de Paus", 11, System.Drawing.Image.FromFile(basePath + "imagens/2.png")),
+                    new Carta("Dois de Paus", 2, System.Drawing.Image.FromFile(basePath + "imagens/6.png")),
+                    new Carta("Três de Paus", 3, System.Drawing.Image.FromFile(basePath + "imagens/10.png")),
+                    new Carta("Quatro de Paus", 4, System.Drawing.Image.FromFile(basePath + "imagens/14.png")),
+                    new Carta("Cinco de Paus", 5, System.Drawing.Image.FromFile(basePath + "imagens/18.png")),
+                    new Carta("Seis de Paus", 6, System.Drawing.Image.FromFile(basePath + "imagens/22.png")),
+                    new Carta("Sete de Paus", 7, System.Drawing.Image.FromFile(basePath + "imagens/26.png")),
+                    new Carta("Oito de Paus", 8, System.Drawing.Image.FromFile(basePath + "imagens/30.png")),
+                    new Carta("Nove de Paus", 9, System.Drawing.Image.FromFile(basePath + "imagens/34.png")),
+                    new Carta("Dez de Paus", 10, System.Drawing.Image.FromFile(basePath + "imagens/38.png")),
+                    new Carta("Valete de Paus", 10, System.Drawing.Image.FromFile(basePath + "imagens/42.png")),
+                    new Carta("Rainha de Paus", 10, System.Drawing.Image.FromFile(basePath + "imagens/46.png")),
+                    new Carta("Rei de Paus", 10, System.Drawing.Image.FromFile(basePath + "imagens/50.png")),
+
+                    // Copas
+                    new Carta("Ás de Copas", 11, System.Drawing.Image.FromFile(basePath + "imagens/3.png")),
+                    new Carta("Dois de Copas", 2, System.Drawing.Image.FromFile(basePath + "imagens/7.png")),
+                    new Carta("Três de Copas", 3, System.Drawing.Image.FromFile(basePath + "imagens/11.png")),
+                    new Carta("Quatro de Copas", 4, System.Drawing.Image.FromFile(basePath + "imagens/15.png")),
+                    new Carta("Cinco de Copas", 5, System.Drawing.Image.FromFile(basePath + "imagens/19.png")),
+                    new Carta("Seis de Copas", 6, System.Drawing.Image.FromFile(basePath + "imagens/23.png")),
+                    new Carta("Sete de Copas", 7, System.Drawing.Image.FromFile(basePath + "imagens/27.png")),
+                    new Carta("Oito de Copas", 8, System.Drawing.Image.FromFile(basePath + "imagens/31.png")),
+                    new Carta("Nove de Copas", 9, System.Drawing.Image.FromFile(basePath + "imagens/35.png")),
+                    new Carta("Dez de Copas", 10, System.Drawing.Image.FromFile(basePath + "imagens/39.png")),
+                    new Carta("Valete de Copas", 10, System.Drawing.Image.FromFile(basePath + "imagens/43.png")),
+                    new Carta("Rainha de Copas", 10, System.Drawing.Image.FromFile(basePath + "imagens/47.png")),
+                    new Carta("Rei de Copas", 10, System.Drawing.Image.FromFile(basePath + "imagens/51.png")),
+
+                    // Ouros
+                    new Carta("Ás de Ouros", 11, System.Drawing.Image.FromFile(basePath + "imagens/4.png")),
+                    new Carta("Dois de Ouros", 2, System.Drawing.Image.FromFile(basePath + "imagens/8.png")),
+                    new Carta("Três de Ouros", 3, System.Drawing.Image.FromFile(basePath + "imagens/12.png")),
+                    new Carta("Quatro de Ouros", 4, System.Drawing.Image.FromFile(basePath + "imagens/16.png")),
+                    new Carta("Cinco de Ouros", 5, System.Drawing.Image.FromFile(basePath + "imagens/20.png")),
+                    new Carta("Seis de Ouros", 6, System.Drawing.Image.FromFile(basePath + "imagens/24.png")),
+                    new Carta("Sete de Ouros", 7, System.Drawing.Image.FromFile(basePath + "imagens/28.png")),
+                    new Carta("Oito de Ouros", 8, System.Drawing.Image.FromFile(basePath + "imagens/32.png")),
+                    new Carta("Nove de Ouros", 9, System.Drawing.Image.FromFile(basePath + "imagens/36.png")),
+                    new Carta("Dez de Ouros", 10, System.Drawing.Image.FromFile(basePath + "imagens/40.png")),
+                    new Carta("Valete de Ouros", 10, System.Drawing.Image.FromFile(basePath + "imagens/44.png")),
+                    new Carta("Rainha de Ouros", 10, System.Drawing.Image.FromFile(basePath + "imagens/48.png")),
+                    new Carta("Rei de Ouros", 10, System.Drawing.Image.FromFile(basePath + "imagens/52.png"))
+                };
+
+                Embaralhar();
+            }
+
+            // Método para embaralhar o baralho
+            public void Embaralhar()
+            {
+                cartas = cartas.OrderBy(x => random.Next()).ToList();
+            }
+
+            // Método para distribuir uma carta, reabastecendo o baralho se estiver vazio
+            public Carta DistribuirCarta()
+            {
+                if (cartas.Count == 0)
+                {
+                    ReporCartas(); // Reabastece o baralho se estiver vazio
+                }
+
+                int index = random.Next(cartas.Count);
+                Carta carta = cartas[index];
+                cartas.RemoveAt(index);
+                return carta;
+            }
+        }
+
+        private List<Carta> maoJogador = new List<Carta>();
+        private List<Carta> maoDealer = new List<Carta>();
+        private Baralho baralho;
+
+        public Form1()
+        {
+            InitializeComponent();
+            baralho = new Baralho();
+            IniciarJogo();
+        }
+        private void IniciarJogo()
+        {
+            maoJogador.Clear();
+            maoDealer.Clear();
+            maoJogador.Add(baralho.DistribuirCarta());
+            maoJogador.Add(baralho.DistribuirCarta());
+            maoDealer.Add(baralho.DistribuirCarta());
+            AtualizarTela();
+        }
+
+        private void AtualizarTela()
+        {
+            string basePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
+            flp_mao_jogador.Controls.Clear();
+            flp_mao_dealer.Controls.Clear();
+
+            // Atualiza as cartas do jogador
+            foreach (var carta in maoJogador)
+            {
+                PictureBox pb = new PictureBox();
+                pb.Image = carta.Imagem;
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                pb.Size = new Size(60, 90);
+                flp_mao_jogador.Controls.Add(pb);
+            }
+            lb_mao_jogador.Text = "Cartas Jogador: " + string.Join(", ", maoJogador.Select(c => c.Nome));
+
+            // Atualiza os pontos do jogador
+            int pontosJogador = CalcularPontos(maoJogador);
+            lb_pontos_jogador.Text = "Pontos Jogador: " + pontosJogador;
+
+            // Atualiza as cartas do dealer
+            for (int i = 0; i < maoDealer.Count; i++)
+            {
+                PictureBox pb = new PictureBox();
+                pb.Size = new Size(60, 90);
+
+                // Mostra a primeira carta do dealer e oculta as outras até o "stand"
+                if (i == 0 || maoDealer.Count > 1)
+                {
+                    pb.Image = maoDealer[i].Imagem;
+                }
+                else
+                {
+                    pb.Image = System.Drawing.Image.FromFile(basePath + "imagens/0.png");
+                }
+
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                flp_mao_dealer.Controls.Add(pb);
+            }
+            lb_mao_dealer.Text = "Cartas Dealer: " + maoDealer[0].Nome + ", [Carta Oculta]";
+
+            int pontosDealerParcial = maoDealer[0].Valor;
+            
+
+            // Se o jogador terminou a jogada (clicou em Stand), mostra as cartas e pontos completos do dealer
+            if (maoDealer.Count > 1)
+            {
+                lb_mao_dealer.Text = "Cartas Dealer: " + string.Join(", ", maoDealer.Select(c => c.Nome));
+                int pontosDealer = CalcularPontos(maoDealer);
+                
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_hit_Click(object sender, EventArgs e)
+        {
+            maoJogador.Add(baralho.DistribuirCarta());
+            if (CalcularPontos(maoJogador) > 21)
+            {
+                MessageBox.Show("Você perdeu! Estourou 21.");
+                IniciarJogo();
+            }
+            AtualizarTela();
+        }
+
+        private void btn_stand_Click(object sender, EventArgs e)
+        {
+            while (CalcularPontos(maoDealer) < 17)
+            {
+                maoDealer.Add(baralho.DistribuirCarta());
+            }
+            VerificarVencedor();
+            AtualizarTela();
+        }
+
+        private int CalcularPontos(List<Carta> mao)
+        {
+            int pontos = mao.Sum(c => c.Valor);
+            int ases = mao.Count(c => c.Nome == "Ás");
+            while (pontos > 21 && ases > 0)
+            {
+                pontos -= 10;
+                ases--;
+            }
+            return pontos;
+        }
+
+        private void VerificarVencedor()
+        {
+            int pontosJogador = CalcularPontos(maoJogador);
+            int pontosDealer = CalcularPontos(maoDealer);
+
+            if (pontosDealer > 21 || pontosJogador > pontosDealer)
+            {
+                MessageBox.Show("Você ganhou!");
+            }
+            else if (pontosJogador == pontosDealer)
+            {
+                MessageBox.Show("Empate!");
+            }
+            else
+            {
+                MessageBox.Show("Você perdeu!");
+            }
+            IniciarJogo();
+        }
+
+        private void flp_mao_dealer_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            IniciarJogo();
+            MessageBox.Show("O jogo foi reiniciado!");
+        }
+    }
+}
